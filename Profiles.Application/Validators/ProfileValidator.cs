9fbf2769055524a5e401648292810ad1e2f5dbf0
Maver1ck1111 <borisovd885@gmail.com>
2025-07-13 23:24:43 +0300

    added fluent validation

---
+++ Profiles.Application/Validators/ProfileValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Application.Validators
+{
+    internal class ProfileValidator
+    {
+    }
+}
