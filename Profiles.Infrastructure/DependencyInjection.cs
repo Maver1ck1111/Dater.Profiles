1847bb0a218f0909dcdc124503b4fac37d4a5285
Maver1ck1111 <borisovd885@gmail.com>
2025-07-13 18:30:11 +0300

    added tests for profile repository

---
+++ Profiles.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Profiles.Application.RepositoriesContracts;
+using Profiles.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProfileRepository, ProfileRepository>();
+            services.AddScoped<ProfilesDbContext>();
+
+            return services;
+        }
+    }
+}
